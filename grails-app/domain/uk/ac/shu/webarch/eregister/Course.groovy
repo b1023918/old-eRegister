e30240b6c12eb13b4efbe4d095ac76cfbf85ca03
student <student@WebArch.localdomain>
2013-05-09 01:43:22 +0100

    created course domain

--- grails-app/domain/uk/ac/shu/webarch/eregister/Course.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Course.groovy
@@ -2,6 +2,22 @@ package uk.ac.shu.webarch.eregister
 
 class Course {
 
+String courseName
+String courseCode
+String description
+
+Set Classes
+
     static constraints = {
+	courseCode maxSize: 20
     }
+static hasMany = [classes: RegClass]
+static mappedBy = [classes: 'course']
+
+static mapping = {
+table 'course'
+courseName column: 'course_name'
+courseCode column: 'mapped_course_code'
+description column: 'course_desc', type:'text'
+}
 }
