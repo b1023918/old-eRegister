0e882e0efc644b37a36fb31f8a994716fc3aaa90
student <student@WebArch.localdomain>
2013-05-09 03:37:09 +0100

    grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy

--- grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
@@ -1,9 +1,24 @@
 package uk.ac.shu.webarch.eregister
 
+
 class Instructor {
 
-String fullInstructorName    
-String instructorNumber
-static constraints = {
-    }
+
+  String name
+  String staffId
+
+
+
+  Set classes
+
+ 
+  static hasMany = [classes: RegClass]
+
+  static mappedBy = [classes: 'classInstructor']
+
+  static constraints = {
+    name(nullable:false, blank:false,maxSize:256);
+    staffId(nullable:false, blank:false,maxSize:20);
+  }
 }
+
