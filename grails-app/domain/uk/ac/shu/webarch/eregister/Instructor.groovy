744efd245486d3f6083ebd1cb41c8ef31cddf015
b1023918 <b1023918@my.shu.ac.uk>
2013-05-09 04:32:55 +0200

    Update Instructor.groovy

--- grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
@@ -1,9 +1,23 @@
 package uk.ac.shu.webarch.eregister
 
+
 class Instructor {
 
-String fullInstructorName    
-String instructorNumber
-static constraints = {
-    }
+
+  String name
+  String staffId
+
+
+
+  Set classes
+
+ 
+  static hasMany = [classes: RegClass]
+
+  static mappedBy = [classes: 'classInstructor']
+
+  static constraints = {
+    name(nullable:false, blank:false,maxSize:256);
+    staffId(nullable:false, blank:false,maxSize:20);
+  }
 }
